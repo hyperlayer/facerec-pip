1190fd67eb152d208bcce89694afbc731c59dbde
jkeifer <jarrettkeifer@gmail.com>
2014-11-04 16:36:59 -0800

    Fixed wrong os command

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open(os.path.join(os.path.dirname(__file__), "README.md")) as readme:
     README = readme.read()
 
 # allow setup.py to be run from any path
-os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.partdir)))
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name = 'facerec',
